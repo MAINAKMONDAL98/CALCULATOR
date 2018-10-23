9daed25e24276737bc2644aeea19c22a7dcf9382
MAINAK MONDAL <32256636+MAINAKMONDAL98@users.noreply.github.com>
2018-10-24 00:27:39 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,96 @@
+
+#include <Keypad.h>
+
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys [ROWS] [COLS] = {
+  {'1', '2', '3', '/'},
+  {'4', '5', '6', '*'},
+  {'7', '8', '9', '-'},
+  {'C', '0', '=', '+'}
+};
+byte rowPins[ROWS] = {13 ,12 ,11 ,10};
+byte colPins[COLS] = {9, 8, 7, 6};
+
+Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+boolean presentValue = false;
+boolean next = false;
+boolean final = false;
+String num1, num2;
+int answer;
+char op;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  
+  delay(5000);
+  
+  delay(5000);
+ 
+}
+
+void loop(){
+  char key = myKeypad.getKey();
+
+  if (key != NO_KEY && (key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9'||key=='0'))
+  {
+    if (presentValue != true)
+    {
+      num1 = num1 + key;
+      int numLength = num1.length();
+   
+      Serial.println(num1); 
+    }
+    else 
+    {
+      num2 = num2 + key;
+      int numLength = num2.length();
+      
+      Serial.println(num2); 
+      final = true;
+    }
+  }
+
+  else if (presentValue == false && key != NO_KEY && (key == '/' || key == '*' || key == '-' || key == '+'))
+  {
+    if (presentValue == false)
+    {
+      presentValue = true;
+      op = key;
+      
+      Serial.println(op);
+    }
+  }
+
+  else if (final == true && key != NO_KEY && key == '='){
+    if (op == '+'){
+      answer = num1.toInt() + num2.toInt();
+    }
+    else if (op == '-'){
+      answer = num1.toInt() - num2.toInt();
+    }
+    else if (op == '*'){
+      answer = num1.toInt() * num2.toInt();
+    }
+    else if (op == '/'){
+      answer = num1.toInt() / num2.toInt();
+    }    
+      
+      Serial.println(answer);
+  }
+  else if (key != NO_KEY && key == 'C'){
+   
+    presentValue = false;
+    final = false;
+    num1 = "";
+    num2 = "";
+    answer = 0;
+    op = ' ';
+  }
+}
